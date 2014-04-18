ff9222f2f9ebdfe38737787b9d1fb6e9cbd77e23
v.backx@gmail.com <v.backx@gmail.com>
2014-04-18 12:28:49 +0200

    commit
    gameover dist

--- resources/js/GameOverLabel.js
+++ resources/js/GameOverLabel.js
@@ -3,7 +3,7 @@ function GameOverLabel () {
 	this.image = new Image();
 	this.image.src = "resources/images/gameover.png";
 
-	this.x = ( canvasWidth - this.image.width * ratio ) /2 +oldheroX;
+	this.x = ( canvasWidth - this.image.width * ratio ) /2 + conteurctx;
 	this.y   =(canvasHeight - this.image.height * ratio) / 2;
 
 	this.w = this.image.width * ratio;
@@ -11,5 +11,17 @@ function GameOverLabel () {
 }
 
 GameOverLabel.prototype.draw = function() {
-	context.drawImage(this.image,0, 0,this.image.width,this.image.height,this.x,this.y,this.w,this.height);
+
+
+	context.drawImage(
+		this.image,
+		0, 
+		0,
+		this.image.width,
+		this.image.height,
+		( canvasWidth - this.image.width * ratio ) /2 + conteurctx,
+		(canvasHeight - this.image.height * ratio) / 2,
+		this.image.width * ratio,
+		this.image.height * ratio
+		);
 };
